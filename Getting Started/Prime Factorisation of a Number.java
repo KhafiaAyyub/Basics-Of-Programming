b7163175f3fdc8fa69f6679e101c50651238a0d4
KhafiaAyyub <85446654+KhafiaAyyub@users.noreply.github.com>
2021-07-25 22:56:19 +0530

    Update Prime Factorisation of a Number.java

--- Getting Started/Prime Factorisation of a Number.java
+++ Getting Started/Prime Factorisation of a Number.java
@@ -1,18 +1,45 @@
 /* Prime Factorisation Of A Number
-Easy  Prev   Next
+Easy  
+
 1. You are required to display the prime factorization of a number.
-    2. Take as input a number n.
-    3. Print all its prime factors from smallest to largest.
+2. Take as input a number n.
+3. Print all its prime factors from smallest to largest.
                                
 Input Format
 n, an integer
+
 Output Format
 p1  p2  p3  p4.. all prime factors of n
-Question Video
 
-  COMMENTConstraints
+Constraints
 2 <= n < 10 ^ 9
+
 Sample Input
 1440
+
 Sample Output
 2 2 2 2 2 3 3 5
+
+*/
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
+  int n = sc.nextInt();
+  
+  for(int div = 2;div*div<=n;div++){
+      while(n%div == 0){
+    
+        System.out.print(div + " ");
+            n = n/div;
+        }  
+      
+    }
+    if(n!=1){
+        System.out.print(n);
+    }
+  
+    }
+}
