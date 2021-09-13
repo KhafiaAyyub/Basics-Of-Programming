cfdfb5dfc298462d45daf6df3bdc26037ef9159a
Khafia Ayyub <85446654+KhafiaAyyub@users.noreply.github.com>
2021-09-13 14:36:12 +0530

    Update Pattern7.java

--- Patterns/Pattern7.java
+++ Patterns/Pattern7.java
@@ -4,10 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
-        
+        int n = scn.nextInt();  
         int sp = 0;
-        
         for(int i =1;i<=n;i++){
             for(int j =1;j<=sp;j++){
                 System.out.print("\t");
@@ -16,7 +14,5 @@ public class Main {
             sp++;
             System.out.println();
         }
-        
-
     }
 }
