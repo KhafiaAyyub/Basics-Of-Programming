1d870b63b1383dcb098db31cefb86ab24765db41
Khafia Ayyub <85446654+KhafiaAyyub@users.noreply.github.com>
2021-09-13 11:49:42 +0530

    Update Pattern2.java

--- Patterns/Pattern2.java
+++ Patterns/Pattern2.java
@@ -4,16 +4,32 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-
-         int n = scn.nextInt();
-        
+        int n = scn.nextInt();
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n-i+1;j++){
                 System.out.print("*\t");
             }
             System.out.println();
-
         }
+    }
+}
 
+//another method
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int nst = n;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=nst;j++){
+                System.out.print("*\t");
+            }
+            nst--;
+            System.out.println();
+        }
     }
 }
