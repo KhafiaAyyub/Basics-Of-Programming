026d88be677778ba30f2df9e85f3098638c558cf
Khafia Ayyub <85446654+KhafiaAyyub@users.noreply.github.com>
2021-09-13 11:46:25 +0530

    Update Pattern1.java

--- Patterns/Pattern1.java
+++ Patterns/Pattern1.java
@@ -4,9 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-
         int n = scn.nextInt();
-        
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
                 System.out.print("*\t");
@@ -14,6 +12,26 @@ public class Main {
             System.out.println();
 
         }
-
     }
 }
+
+//another method
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int nst = 1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=nst;j++){
+                System.out.print("*\t");
+            }
+            nst++;
+            System.out.println();
+        }
+    }
+}
+
